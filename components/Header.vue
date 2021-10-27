c1cc46121327bec8470e650d046a278031b9f10d
JA EM <james@devfong.co.th>
2021-10-27 18:09:38 +0700

    add_menu_button

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,106 @@
 <template>
   <div>
-    <v-app-bar color="primary" dark fixed app
+    <!-- <v-app-bar color="primary" dark fixed app
       ><v-spacer></v-spacer>
-      <h2 class="mt-3 font-weight-regular">
+      <h2 class="my-1 font-weight-regular">
         <v-icon left :size="iconSize">{{ icon }}</v-icon>
         {{ title }}
       </h2>
       <v-spacer></v-spacer
-    ></v-app-bar>
-    <v-system-bar v-show="systemBar" color="secondary" lights-out dark>
-      <v-spacer></v-spacer>
-      <v-btn x-small rounded color="primary">รายงาน</v-btn>
-      <v-btn x-small rounded color="primary">ลูกค้า</v-btn>
+    ></v-app-bar> -->
+    <v-app-bar
+      color="primary"
+      dark
+      fixed
+      app
+      :prominent="prominent"
+      :dense="dense"
+    >
+      <v-col align="center" class="ma-0 my-2 pa-0">
+        <v-row justify="center" class="">
+          <h2 class="font-weight-regular">
+            <v-icon left :size="iconSize">{{ icon }}</v-icon>
+            {{ title }}
+          </h2>
+        </v-row>
+        <v-row v-show="systemBar" justify="center" class="mt-n3">
+          <v-btn-toggle
+            v-model="slide"
+            tile
+            color="error"
+            group
+            class="hidden-sm-and-down"
+          >
+            <!-- <v-btn value="left"> รายงาน </v-btn>
 
+            <v-btn value="center"> ลูกค้า </v-btn>
+
+            <v-btn value="right"> สิทธิ์เข้าถึง </v-btn>
+
+            <v-btn value="justify"> ออกจากระบบ </v-btn> -->
+            <v-menu v-for="i in 4" :key="i" offset-y>
+              <template #activator="{ on, attrs }">
+                <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                  {{ i }}
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item v-for="(item, index) in items" :key="index" link>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-btn-toggle>
+          <v-slide-group
+            v-model="slide"
+            show-arrows
+            class="mt-3 hidden-md-and-up"
+          >
+            <v-slide-item>
+              <v-btn
+                class="mx-2"
+                :input-value="1"
+                active-class="error white--text"
+                depressed
+                rounded
+              >
+                รายงาน
+              </v-btn>
+            </v-slide-item>
+            <v-slide-item>
+              <v-btn
+                class="mx-2"
+                active-class="error white--text"
+                depressed
+                rounded
+              >
+                ลูกค้า
+              </v-btn>
+            </v-slide-item>
+          </v-slide-group>
+        </v-row>
+      </v-col>
+      <!-- <v-col>
+        <v-btn outlined color="secondary">รายงาน</v-btn>
+        <v-btn outlined color="secondary">ลูกค้า</v-btn>
+      </v-col> -->
+    </v-app-bar>
+    <!-- <v-system-bar v-show="systemBar" color="secondary" lights-out dark>
+      <v-spacer></v-spacer>
+      <v-btn x-small outlined color="primary">รายงาน</v-btn>
+      <v-btn x-small outlined color="primary">ลูกค้า</v-btn>
+      <v-btn-toggle v-model="text" tile color="primary" group>
+        <v-btn id="no-background-hover" value="left" ripple> Left </v-btn>
+
+        <v-btn value="center"> Center </v-btn>
+
+        <v-btn value="right"> Right </v-btn>
+
+        <v-btn value="justify"> Justify </v-btn>
+      </v-btn-toggle>
       <span>12:30</span>
       <v-spacer></v-spacer>
-    </v-system-bar>
+    </v-system-bar> -->
   </div>
 </template>
 <script>
@@ -33,6 +118,25 @@ export default {
       type: Boolean,
       default: false,
     },
+    prominent: {
+      type: Boolean,
+      default: false,
+    },
+    dense: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      slide: 1,
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+    }
   },
   computed: {
     iconSize() {
@@ -48,3 +152,4 @@ export default {
   },
 }
 </script>
+<style scoped></style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 100%">
-    <Header title="เข้าสู่ระบบ" />
+    <Header title="เข้าสู่ระบบ" icon="mdi-account-lock" />
     <v-row justify="center" align="center" class="fill-height">
       <v-col cols="1" sm="2" md="3" lg="4"></v-col>
       <v-col cols="10" sm="8" md="6" lg="4" align="center">
@@ -18,7 +18,13 @@
           </v-col>
         </v-row>
         <v-row>
-          <v-btn rounded block color="primary">เข้าสู่ระบบ</v-btn>
+          <v-btn
+            rounded
+            block
+            color="primary"
+            @click="$router.push('/admin/home')"
+            >เข้าสู่ระบบ</v-btn
+          >
         </v-row>
       </v-col>
       <v-col cols="1" sm="2" md="3" lg="4"></v-col>
