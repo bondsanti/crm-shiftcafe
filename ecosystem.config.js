f18d6d5489e513c13d7c1e7b36487247455ea50d
jamesmy <56800502+jamesmy@users.noreply.github.com>
2021-11-02 15:24:13 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
-export default {
+module.exports = {
   apps: [
     {
-      name: 'crm_for_shiftcafe',
+      name: 'SHIFT CAFÉ',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
-    },
-  ],
+      args: 'start'
+    }
+  ]
 }
