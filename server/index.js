d69818a726c064a1f6075430542b2138f7f54d5a
JA EM <james@devfong.co.th>
2021-11-02 11:24:46 +0700

    fix_nuxt_config

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
-
+import cors from 'cors'
 import indexRoute from './routes'
 import errorHandler from './middleware/error.middleware'
 
 const app = express()
 app.use(express.urlencoded())
 app.use(express.json({ limit: '20mb' }))
-
+app.use(cors())
 app.use('/', indexRoute)
 app.use(errorHandler)
 
