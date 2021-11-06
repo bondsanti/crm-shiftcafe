81a0b526fdbaece16e4aa9f390dd9f9c7bbf016c
JA EM at second floor <james_for_work@example.com>
2021-11-07 01:14:15 +0700

    improve_table

--- pages/admin/report.vue
+++ pages/admin/report.vue
@@ -3,7 +3,11 @@
     <Header title="Dashboard" system-bar icon="mdi-chart-box" prominent dense />
     <v-row dense class="ma-2">
       <v-col cols="12" sm="6" lg="3">
-        <AdminTopCard title="สรุปยอดขาย" route-name="admin-report" />
+        <AdminTopCard
+          title="สรุปยอดขาย"
+          route-name="admin-report"
+          to="/admin/report"
+        />
       </v-col>
       <!-- <v-col cols="12" sm="6" lg="3">
         <AdminTopCard
@@ -22,20 +26,21 @@
           title="ใบเสร็จรับเงิน"
           icon="mdi-receipt"
           route-name="admin-report-receipt"
+          to="/admin/report/receipt"
         />
       </v-col>
     </v-row>
-    <nuxt-child></nuxt-child>
-    <!-- <v-row class="ma-3">
-      <AdminTable
-        title="สรุปยอดขาย"
-        icon="mdi-cash"
-        :headers="headers"
-        :items="items"
-        :items-sub-header="itemsSubHeader"
-        :loading="loading"
-        @getDateRange="getDateRange"
-      />
-    </v-row> -->
+    <nuxt-child :all-receipts="receipts"></nuxt-child>
   </div>
 </template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const result = await $axios.$get('/receipt')
+    // const { receipts } = result
+    // console.log(result)
+    // const receipt2 = result.filter((r) => r.cancelled_at === null)
+    return { receipts: result }
+  },
+}
+</script>
--- pages/admin/report/receipt.vue
+++ pages/admin/report/receipt.vue
@@ -1,3 +1,135 @@
 <template>
-  <h1>receipt</h1>
+  <v-row class="ma-3">
+    <AdminTable
+      title="ใบเสร็จรับเงิน"
+      icon="mdi-cash"
+      btn
+      :headers="headers"
+      :items="items"
+      :items-sub-header="itemsSubHeader"
+      :loading="loading"
+      @getDateRange="getDateRange"
+    />
+  </v-row>
 </template>
+<script>
+export default {
+  props: {
+    allReceipts: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  data: () => ({
+    headers: [
+      {
+        text: 'เลขที่ใบเสร็จรับเงิน.',
+        align: 'start',
+        sortable: false,
+        value: 'no',
+      },
+      {
+        text: 'วันที่',
+        align: 'start',
+        sortable: false,
+        value: 'date',
+      },
+      { text: 'พนักงาน', align: 'start', sortable: false, value: 'employee' },
+      { text: 'ลูกค้า', align: 'start', sortable: false, value: 'customer' },
+      {
+        text: 'ชนิด',
+        align: 'start',
+        sortable: false,
+        value: 'type',
+      },
+      {
+        text: 'รวมทั้งหมด',
+        align: 'start',
+        sortable: false,
+        value: 'total',
+      },
+    ],
+    itemsSubHeader: [
+      {
+        value: 100,
+        text: 'ใบเสร็จรับเงินทั้งหมด',
+        icon: 'mdi-receipt',
+      },
+      {
+        value: 2,
+        text: 'ยอดขาย',
+        icon: 'mdi-cash',
+      },
+      {
+        value: 3,
+        text: 'คืนเงิน',
+        icon: 'mdi-cash-refund',
+      },
+      {
+        value: 4,
+        text: 'ยกเลิก',
+        icon: 'mdi-cash-remove',
+      },
+    ],
+    items: [],
+    loading: false,
+  }),
+  head() {
+    return {
+      title: 'ใบเสร็จรับเงิน',
+    }
+  },
+  created() {
+    // console.log(this.title)
+    // this.receipts = this.allReceipts.filter((r) => r.cancelled_at === null)
+  },
+  methods: {
+    getDateRange(dayList) {
+      this.loading = true
+      let dates = []
+      this.items = []
+      dayList.reverse().map((d) => {
+        const res = this.filterReceiptsByDate(d)
+        dates = [...dates, ...res]
+        return res
+      })
+      // หน่วงเวลา
+      console.log(dates)
+
+      setTimeout(() => {
+        dates.map((d) => {
+          this.makeItRightForTable(d)
+          return d
+        })
+        this.loading = false
+        // this.calculate()
+      }, 1200)
+    },
+
+    filterReceiptsByDate(date) {
+      // console.log(new Date(date))
+      const result = this.allReceipts.filter((r) => {
+        return (
+          new Date(r.receipt_date).getDate() === new Date(date).getDate() &&
+          new Date(r.receipt_date).getMonth() === new Date(date).getMonth() &&
+          new Date(r.receipt_date).getFullYear() ===
+            new Date(date).getFullYear()
+        )
+      })
+      return result
+    },
+    makeItRightForTable(data) {
+      const obj = {
+        no: data.receipt_number,
+        date: this.$options.filters.dateTh(data.receipt_date),
+        employee: data.employee_id,
+        customer: data.customer_id || '-',
+        type: data.receipt_type,
+        total: this.$options.filters.currency(data.total_money),
+      }
+      this.items.push(obj)
+    },
+  },
+}
+</script>
